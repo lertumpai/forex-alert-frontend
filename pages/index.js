6fd85c39103dd29078112cf3925343b66d2d76e1
ninew <new-sorawit@outlook.com>
2021-05-28 20:58:11 +0700

    setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const IndexPage = () => {
+  return (
+    <div className='bg-authentication'>
+
+    </div>
+  )
+}
+
+export default IndexPage
