0ca757fcd7cdd2984e59a15438576ffbc0a4cd2c
ninew <new-sorawit@outlook.com>
2021-05-29 21:40:21 +0700

    bump version

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
+
+import pkg from '../package.json'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Memorize version {pkg.version}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
